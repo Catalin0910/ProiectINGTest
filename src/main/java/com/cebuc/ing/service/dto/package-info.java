75032544537f07e93682b4962098f040cfaab682
unknown <catalincebuc99@gmail.com>
2022-06-06 11:07:52 +0300

    all components for project

---
+++ src/main/java/com/cebuc/ing/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.cebuc.ing.service.dto;
---
+++ src/test/java/com/cebuc/ing/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.cebuc.ing.cucumber;
+
+import com.cebuc.ing.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
